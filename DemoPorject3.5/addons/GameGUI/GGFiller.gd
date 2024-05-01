25444a553aec80c4d151e6b7e1c398d4ba800616
baiXfeng <394811161@qq.com>
2024-05-02 03:05:12 +0800

    GGFiller

--- DemoPorject3.5/addons/GameGUI/GGFiller.gd
+++ DemoPorject3.5/addons/GameGUI/GGFiller.gd
@@ -1,4 +1,4 @@
-@tool
+tool
 
 ## A GameGUI node that by default will expand to fill any extra space within a layout.
 ## There is no functional difference between [GGFiller] and [GGComponent], but the name [GGFiller]
